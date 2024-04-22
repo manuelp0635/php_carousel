105ae9bde2c3fee8a9ee494d85754fd2165c28a7
Manuel Ramos <92071461+manuelp0635@users.noreply.github.com>
2024-04-22 18:53:35 -0500

    Update index.php
    Actualizacion del index.php

--- index.php
+++ index.php
@@ -12,12 +12,21 @@ include_once "./configuracion/configuracion.php";
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
+
+<!-- Star body-->
 <body>
+	<br><br>
+
+<!-- Star Div -->
 <div class="containers">
 	<?php
 	myCarousel("all", $data);
 	?>
 </div>
+<!-- End Div -->
+<br><br><br>
+
+<!-- Div con texto -->
 <div class="container" style="text-align:center;">
 	<h2>Carousel configurado en Bootstrap - PHP - Json</h2>
 	<p>
@@ -27,8 +36,18 @@ include_once "./configuracion/configuracion.php";
 		Tan solo basta con subir las fotografias al directorio que el programa las desplegará.
 	</p>
 </div>
-<?php
-myCarousel("all", $data_b);
-?>
+<!-- End Div -->
+
+ <!-- Start br-->
+ <br><br><br><br>
+ <!-- End br-->
+
+  <!--Start Footer -->
+  <center><footer class="container" >
+    <!--<p class="float-end"><a href="#">Back to top</a></p>-->
+    <p>&copy; Derechos reservados 2020–2024 Company. </p>
+  </footer></center>
+  <!-- End Footer -->
+
 </body>
 </html>
